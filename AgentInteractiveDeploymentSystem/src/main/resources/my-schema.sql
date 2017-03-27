e0b2131e4fe0e0b5ebb0e4c195d612ced9056c89
xmatusk3 <445561@mail.muni.cz>
2017-03-27 23:39:43 +0200

    script fur database creating

---
+++ AgentInteractiveDeploymentSystem/src/main/resources/my-schema.sql
@@ -0,0 +1,24 @@
+-- my-schema.sql
+
+CREATE TABLE missions (
+  id                     BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  description            VARCHAR(300),
+  numberOfRequiredAgents SMALLINT,
+  difficulty             INT,
+  place                  VARCHAR(100),
+  successful             BOOLEAN);
+
+CREATE TABLE agents (
+  id          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name        VARCHAR(150),
+  gender      SMALLINT,
+  age         SMALLINT,
+  phoneNumber VARCHAR(20),
+  alive       BOOLEAN);
+
+CREATE TABLE assignments (
+  id           BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  AgentId      BIGINT REFERENCES agents (id) ON DELETE CASCADE,
+  MissionId    BIGINT REFERENCES missions (id) ON DELETE CASCADE,
+  start        TIMESTAMP,
+  "end"        TIMESTAMP);
