eec97bc87ebd5fcd3ad93cd24911846392fe0fef
xmatusk3 <445561@mail.muni.cz>
2017-03-27 23:38:32 +0200

    Addid agent validation in metods, chejngd phone tu phoneNumber in SQL queris

--- AgentInteractiveDeploymentSystem/src/main/java/cz/muni/fi/ManagersImpl/AgentManagerImpl.java
+++ AgentInteractiveDeploymentSystem/src/main/java/cz/muni/fi/ManagersImpl/AgentManagerImpl.java
@@ -2,6 +2,7 @@ package cz.muni.fi.ManagersImpl;
 
 import cz.muni.fi.Base.Agent;
 import cz.muni.fi.Managers.AgentManager;
+import cz.muni.fi.common.ValidationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -29,14 +30,15 @@ public class AgentManagerImpl implements AgentManager{
             agent.setName(resultSet.getString("name"));
             agent.setGender(resultSet.getShort("gender"));
             agent.setAge(resultSet.getShort("age"));
-            agent.setPhoneNumber(resultSet.getString("phone"));
+            agent.setPhoneNumber(resultSet.getString("phoneNumber"));
             agent.setAlive(resultSet.getBoolean("alive"));
             return agent;
         }
     };
 
     @Override
-    public void createAgent(Agent agent) {
+    public void createAgent(Agent agent) throws IllegalArgumentException, ValidationException {
+        validate(agent);
         SimpleJdbcInsert insertCustomer = new SimpleJdbcInsert(jdbcTemplate).withTableName("agents").
                 usingGeneratedKeyColumns("id");
 
@@ -44,7 +46,7 @@ public class AgentManagerImpl implements AgentManager{
                 .addValue("name", agent.getName())
                 .addValue("gender", agent.getGender())
                 .addValue("age", agent.getAge())
-                .addValue("phone", agent.getPhoneNumber())
+                .addValue("phoneNumber", agent.getPhoneNumber())
                 .addValue("alive" , agent.isAlive());
 
         Number id = insertCustomer.executeAndReturnKey(parameters);
@@ -52,8 +54,13 @@ public class AgentManagerImpl implements AgentManager{
     }
 
     @Override
-    public Agent findAgentById(long agentId) {
-        return jdbcTemplate.queryForObject("SELECT * FROM agents WHERE id=?", agentMapper, agentId);
+    public Agent findAgentById(Long agentId) throws IllegalArgumentException {
+        if (agentId == null) { throw new IllegalArgumentException("agentId is null"); }
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM agents WHERE id=?", agentMapper, agentId);
+        } catch (org.springframework.dao.DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
@@ -62,18 +69,50 @@ public class AgentManagerImpl implements AgentManager{
     }
 
     @Override
-    public void updateAgent(Agent agent) {
-        jdbcTemplate.update("UPDATE agents SET name=?,gender=?,age=?,phone=?,alive=? WHERE id=?",
-                agent.getName(), agent.getGender(),agent.getAge(),agent.getPhoneNumber(),agent.isAlive());
+    public void updateAgent(Agent agent) throws IllegalArgumentException, ValidationException{
+        validate(agent);
+        jdbcTemplate.update("UPDATE agents SET name=?,gender=?,age=?,phoneNumber=?,alive=? WHERE id=?",
+                agent.getName(), agent.getGender(),agent.getAge(),agent.getPhoneNumber(),agent.isAlive(), agent.getId());
     }
 
     @Override
-    public void deleteAgent(Agent agent) {
+    public void deleteAgent(Agent agent) throws IllegalArgumentException, ValidationException {
+        validate(agent);
         jdbcTemplate.update("DELETE FROM agents where id=?",agent.getId());
     }
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    private void validate(Agent agent) throws IllegalArgumentException, ValidationException {
+        if (agent == null) {
+            throw new IllegalArgumentException("agent is null");
+        }
+
+        if (agent.getName() == null) {
+            throw new ValidationException("name is null");
+        }
+
+        if (agent.getAge() < 18) {
+            throw new ValidationException("agent age is under 18");
+        }
+
+        if (agent.getGender() < 0) {
+            throw new ValidationException("gender is negative");
+        }
+
+        if (agent.getPhoneNumber() == null) {
+            throw new ValidationException("phone number is null");
+        }
+
+        if (agent.getPhoneNumber().isEmpty()) {
+            throw new ValidationException("phone number is empty");
+        }
+
+        if (agent.getName().isEmpty()) {
+            throw new ValidationException("name is empty");
+        }
     }
 }
